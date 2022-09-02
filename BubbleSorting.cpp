015e5d4a5b7b96809e38d6a262c0bebf2b724c00
Mubashir Iftikhar <mubashiriftikharkhan@gmail.com>
2022-09-02 19:20:34 +0500

    BubbleSort.cpp

---
+++ BubbleSorting.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int size,pass = 0;
+    int array[size] = {};
+    cout<<"Enter the array size: ";
+    cin>>size;
+    for (int i = 0; i < size; i++){
+        cout<<"Enter the array element: ";
+        cin>>array[i];
+    }
+    cout<<endl;
+    for (int i = 0; i < size; i++){
+        for (int j = i + 1; j < size; j++){
+            if (array[i] > array[j]){
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+            
+        }
+        pass++;
+    }
+    cout<<"Bubble Sorted list is: "<<endl;
+    for (int i = 0; i < size; i++){
+        cout<<array[i]<<"\t";
+    }
+        cout<<endl;
+        cout<<"Pass of Sorted list: "<<pass;
+    return 0;
+}
